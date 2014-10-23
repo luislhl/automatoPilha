ac967b094729b41bfd765b5ea65749a08a0dea60
Luis Helder <luislhl@vialink.com.br>
2014-10-23 00:49:02 -0200

    Continuando a fazer a leitura do arquivo

--- src/automatopilha/AutomatoPilha.java
+++ src/automatopilha/AutomatoPilha.java
@@ -1,11 +1,14 @@
 package automatopilha;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Stack;
 
 public class AutomatoPilha {
     ArrayList<String> alfabeto;
-    ArrayList<Estado> estados;
+    
+    Map<String, Estado> estados;
+    
     Estado estadoInicial;
     ArrayList<Estado> estadosFinais;
     ArrayList<String> alfabetoPilha;
--- src/automatopilha/Input.java
+++ src/automatopilha/Input.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package automatopilha;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- *
- * @author helder
- */
 public class Input {
     String alfabeto;
     String estados;
@@ -24,20 +16,58 @@ public class Input {
     
     public AutomatoPilha criarAP(){
         AutomatoPilha automato = new AutomatoPilha();
-        automato.alfabeto = processaAlfabeto();
+        automato.alfabeto = processaAlfabeto(alfabeto);
+        automato.estados = processaEstados();
+        processaFuncaoPrograma(automato);
+        automato.alfabetoPilha = processaAlfabeto(alfabetoPilha);
         
         return automato;
     }
-    
-    private ArrayList<String> processaAlfabeto(){
+     
+    private ArrayList<String> processaAlfabeto(String alf){
         ArrayList<String> alfabeto_retorno = new ArrayList<>();
         
-        String alfabeto = this.alfabeto.replace(" ", "");
+        String alfabeto = alf.replace(" ", "");
         alfabeto = alfabeto.split("=")[1];
         String[] simbolos = alfabeto.split(",");
         alfabeto_retorno.addAll(Arrays.asList(simbolos));
         
         return alfabeto_retorno;
+    }
+    
+    private Map<String, Estado> processaEstados(){
+        Map<String, Estado> estados_list = new HashMap<>();
+        
+        String estados = this.estados.replace(" ", "");
+        estados = estados.split("=")[1];
+        String[] estados_array = estados.split(",");
+        for(String est : estados_array){
+            estados_list.put(est, new Estado(est));
+        }
+        
+        return estados_list;
+    }
+    
+    private void processaFuncaoPrograma(AutomatoPilha automato){
+        String funcao = this.funcaoPrograma.replace(" ", "");
+        funcao = funcao.split("=")[1];
+        String[] transicoes = funcao.split(",");
+        for (String transicao : transicoes){
+            processaTransicao(transicao, automato);
+        }
+    }
+    
+    private void processaTransicao(String transicao, AutomatoPilha automato){
+        String[] lados = transicao.split("->");
+        String[] simbolos_esquerda = lados[0].split(",");
+        String[] simbolos_direita = lados[1].split(",");
+        
+        Estado estado_origem = automato.estados.get(simbolos_direita[0]);
+        Estado estado_destino = automato.estados.get(simbolos_esquerda[1]);
+        
+        Transicao transi = new Transicao(simbolos_direita[1], simbolos_direita[2], simbolos_esquerda[0], estado_destino);
+        estado_origem.transicoes.add(transi);
+        
     }
     
 }
